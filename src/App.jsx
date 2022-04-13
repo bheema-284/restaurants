6a2b02a64eff00f1f8e94226a3bba3abc9bb13ed
bheema-284 <bheemudug@gmail.com>
2022-04-13 22:23:42 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
-
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
+import { Login } from './comonents/Login';
+//import { SignUp } from './comonents/Signup';
+import RestaurantList from './comonents/Home';
+const PrivateRoute = ({ isAuthenticated, children }) => {
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
 function App() {
- 
+  const isAuthenticated = false;
   return (
     <div className="App">
-      Hello
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <RestaurantList />
+            </PrivateRoute>
+          }></Route>
+        {/* <Route path="/signup" element={<SignUp />}></Route> */}
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
